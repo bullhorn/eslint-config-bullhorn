542f3e997c5970f748f182a0a874f361405e728c
Joshua Godi <jgodi@bullhorn.com>
2015-11-16 13:11:15 -0600

    chore(rules): Initial commit for the JS rules (very strict atm)

---
+++ README.md
@@ -0,0 +1,19 @@
+# eslint-config-bullhorn
+
+> ESLint config for all Bullhorn projects
+
+## Installation
+
+Install packages as devDependencies:
+
+```bash
+npm install --save-dev babel-eslint eslint eslint-config-bullhorn
+```
+
+Add a `.eslintrc` config to your project:
+
+```json
+{
+  "extends": "bullhorn"
+}
+```
---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'extends': [
+        'eslint-config-bullhorn/rules/best-practices',
+        'eslint-config-bullhorn/rules/errors',
+        'eslint-config-bullhorn/rules/es6',
+        'eslint-config-bullhorn/rules/node',
+        'eslint-config-bullhorn/rules/strict',
+        'eslint-config-bullhorn/rules/style',
+        'eslint-config-bullhorn/rules/variables'
+    ],
+    'env': {
+        'browser': true
+    }
+};
