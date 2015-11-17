57ba5536a04d325eca0bfea53b77fdc21c04451e
Joshua Godi <jgodi@bullhorn.com>
2015-11-17 08:01:48 -0600

    Updating the eslint config and adding some test eslint configs for angular/non-angular tests.

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ module.exports = {
     ],
     'env': {
         'browser': true
-    }
+    },
+    'globals': {}
 };
--- rules/variables.js
+++ rules/variables.js
@@ -1,24 +1,12 @@
 module.exports = {
     'rules': {
-        'init-declarations': 0,
-        'max-depth': 1,
-        'max-len': 0,
-        'max-params': 0,
-        'max-statements': 0,
-        'no-bitwise': 0,
-        'no-catch-shadow': 0,
+        'no-catch-shadow': 2,
         'no-delete-var': 2,
-        'no-label-var': 0,
-        'no-plusplus': 0,
         'no-shadow-restricted-names': 2,
         'no-shadow': 2,
         'no-undef-init': 2,
         'no-undef': 2,
-        'no-undefined': 2,
-        'no-unused-vars': [2, {
-            'vars': 'local',
-            'args': 'after-used'
-        }],
-        'no-use-before-define': 2
+        'no-undefined': 0,
+        'no-unused-vars': [2, {'args': 'after-used'}]
     }
 };
